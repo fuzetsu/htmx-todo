8416a86aa65e346152bfda4fe7f0f5d15e4b01ac
fuz <fuzetsu@users.noreply.github.com>
2024-03-02 01:49:12 -0500

    add doctype to root.tsx
    - include type defs for htmx

--- src/base/Root.tsx
+++ src/base/Root.tsx
@@ -1,16 +1,19 @@
-import { CSS_PUBLIC_PATH, HTMX_PUBLIC_PATH } from '../constants'
+import { CSS_PUBLIC_PATH, DOCTYPE, HTMX_PUBLIC_PATH } from '../constants'
 
-export function Root({ children }: { children: JSX.Element }) {
+export function Root({ children }: Html.PropsWithChildren) {
   return (
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Todo list</title>
-        <link rel="stylesheet" href={CSS_PUBLIC_PATH} />
-        <script src={HTMX_PUBLIC_PATH} />
-      </head>
-      <body>{children}</body>
-    </html>
+    <>
+      {DOCTYPE}
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <title>Todo list</title>
+          <link rel="stylesheet" href={CSS_PUBLIC_PATH} />
+          <script src={HTMX_PUBLIC_PATH} />
+        </head>
+        <body>{children}</body>
+      </html>
+    </>
   )
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 export const CSS_PUBLIC_PATH = '/public/styles.css'
 export const HTMX_PUBLIC_PATH = '/public/htmx.min.js'
+export const DOCTYPE = '<!DOCTYPE html>'
